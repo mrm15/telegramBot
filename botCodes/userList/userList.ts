714684e25a9ea995c90d3a1aabe4f443430130d0
mrm15 <mrm.learn@gmail.com>
2024-11-09 03:53:38 +0330

    debug

--- botCodes/userList/userList.ts
+++ botCodes/userList/userList.ts
@@ -1,3 +1,5 @@
 export const allowedPhoneNumbers = [
     "+989384642159", // Add your allowed phone numbers in international format
+    "09384642159", // Add your allowed phone numbers in international format
+    "989384642159", // Add your allowed phone numbers in international format
 ];
