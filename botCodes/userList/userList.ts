fc1d12094d74115f8c1cda8a7a8ed42260f7f2db
mohammad mirarab <mrm.learn@gmail.com>
2024-11-14 15:42:30 +0330

    update

--- botCodes/userList/userList.ts
+++ botCodes/userList/userList.ts
@@ -2,4 +2,7 @@ export const allowedPhoneNumbers = [
     "+989384642159", // Add your allowed phone numbers in international format
     "09384642159", // Add your allowed phone numbers in international format
     "989384642159", // Add your allowed phone numbers in international format
+    "989126970541",
+    "09126970541",
+    "+989126970541",
 ];
